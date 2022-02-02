3ba4b8d49b26eb739a29a0b78d396c198e10bcce
Jose Arturo <arturotoll5@gmail.com>
2022-02-02 16:45:56 -0500

    insatalling dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,12 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  },
+  methodOverride: {
+    allowed: ['PUT', 'PATCH', 'DELETE']
+  }
 };
 
 export default config;
