9613821e84d6b75d3542194d58a5fa550fbea277
Jose Arturo <arturotoll5@gmail.com>
2022-02-25 18:48:33 -0500

    change database

--- src/lib/db/connection.js
+++ src/lib/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 async function connection() {
   if (mongoose.connection.readyState === 1) return true
-  //const DATABASE = "mongodb+srv://aronian666:miqevid21@cluster0.h2xqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  const DATABASE = "mongodb+srv://aronian666:miqevid21@cluster0.h2xqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   //const DATABASE = 'mongodb://localhost:27017/glass'
   try {
     await mongoose.connect(DATABASE)
